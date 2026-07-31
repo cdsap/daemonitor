8f30026d8ce9120f664df29b85465cfc280c25ac
Inaki Villar <inaki.seri@gmail.com>
2026-07-31 10:02:25 -0700

    chore: prepare 1.0.2 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ plugins {
 }
 
 group = "io.github.cdsap.daemonitor"
-version = "0.1.1"
+version = "1.0.2"
 
-val nativePackageVersion = "1.0.1"
+val nativePackageVersion = "1.0.2"
 
 val buildInfoDirectory = layout.buildDirectory.dir("generated/build-info")
 val buildCommit = providers.environmentVariable("GITHUB_SHA")
